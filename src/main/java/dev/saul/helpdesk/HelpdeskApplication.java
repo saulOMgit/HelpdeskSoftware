e084213fc15caf525cb8355352b8e41819470936
¨Saul¨ <sauloteromelchor@gmail.com>
2025-08-28 12:51:47 +0200

    entorno de pruebas en HOME

--- src/main/java/dev/saul/helpdesk/HelpdeskApplication.java
+++ src/main/java/dev/saul/helpdesk/HelpdeskApplication.java
@@ -1,7 +1,12 @@
 package dev.saul.helpdesk;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import dev.saul.helpdesk.model.Topic;
+import dev.saul.helpdesk.repository.TopicRepository;
 
 @SpringBootApplication
 public class HelpdeskApplication {
@@ -9,5 +14,17 @@ public class HelpdeskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
+
+	@Bean
+CommandLineRunner initDatabase(TopicRepository repo) {
+    return args -> {
+        if (repo.count() == 0) {
+            repo.save(new Topic("Infraestructura"));
+            repo.save(new Topic("Aplicaciones"));
+            repo.save(new Topic("Soporte"));
+        }
+    };
+}
+
 
 }
